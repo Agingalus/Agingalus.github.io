3c1e54cfacc3ab84f5635354f25e6cabb5990fb5
Agingalus <aaron.greene@bellevuecollage.edu>
2018-11-05 12:16:57 -0800

    javascript

---
+++ JavaScriptTest/functionBasics.js
@@ -0,0 +1,24 @@
+let words = "Hi what is up man!";
+function simpleFunction()
+{
+console.log("Functions should consist of statements designed to perform a single task.")
+}
+
+function functionParamers(words)
+{
+console.log(words);
+}
+function add(a,b)
+{
+    console.log(a + b);
+}
+function adder()
+{
+    console.log(2+3)
+}
+
+
+simpleFunction();
+functionParamers(words);
+add(42,7);
+adder();
