615506c6fb8965c8caf27768d8f79702c816dfd2
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-01-20 22:28:14 -0800

    first project

---
+++ javaScript/Exersise2A.js
@@ -0,0 +1,9 @@
+for (let i = 0; i > 300; i++) {
+    if (i % 5 === 0) {
+        console.log("Fizz");
+    } else if (i % 7 === 0) {
+        console.log("Fuzz")
+    } else {
+        console.log(i + 1)
+    }
+}
