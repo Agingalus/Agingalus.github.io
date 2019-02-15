a8e4a8d984cc05d6d3563e38b716da43ab37a137
Aaron Greene <43617847+Agingalus@users.noreply.github.com>
2019-02-14 17:37:35 -0800

    game

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016
+    },
+    "rules": {
+
+
+        "indent": [
+            "error",
+            4, { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- adventureGame/adventureGame.js
+++ adventureGame/adventureGame.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 "use strict";
 // get elenents
 const printLocation = document.getElementById("location");
@@ -39,9 +40,9 @@ const locationImage = ["images/ballroom.jpg", "images/diningroom.jpg", "images/B
 
 
 
-const wepons = ["lead pipe", "rope", "candle stick", "knife", "wrench", "revolver"];
+//const wepons = ["lead pipe", "rope", "candle stick", "knife", "wrench", "revolver"];
 
-const people = ["Mrs. Peacock", "Mr. Green", "Mrs. White", "Professor Plum", "Coronel Mustard", "Miss Scarlet"];
+//const people = ["Mrs. Peacock", "Mr. Green", "Mrs. White", "Professor Plum", "Coronel Mustard", "Miss Scarlet"];
 
 const items = ["wine bottle", "flute"];
 const itemLocations = [1, 4];
@@ -63,8 +64,8 @@ const errorMessages = ["You can not go north of here, you are at the edge of the
 const actions = ["north", "east", "south", "west", "take secret passage", "help", "take item", "play item", "give item", "unlock door", "talk", "hint"];
 
 //people dont think i need this
-let personRoom = [0, 2, 3, 5, 6, 8];
-let pesonName = ["Miss Scarlet", "Mrs. White", "Mrs. Peacock", "Professor Plum", "Colonel Mustard", "Mr. Green"];
+//let personRoom = [0, 2, 3, 5, 6, 8];
+//let pesonName = ["Miss Scarlet", "Mrs. White", "Mrs. Peacock", "Professor Plum", "Colonel Mustard", "Mr. Green"];
 
 
 
@@ -83,18 +84,18 @@ class Person {
         this.happy = happy;
         this.happyClue = happyClue;
         this.room = room;
-        this.picture = picture
+        this.picture = picture;
         this.notHappyClue = notHappyClue;
 
     }
 
-    name;
-    happy;
-    notHappyClue;
-    happyClue;
-    room;
-    picture;
-};
+    // name;
+    // happy;
+    // notHappyClue;
+    // happyClue;
+    // room;
+    // picture;
+}
 
 let scarlet = new Person("Miss Scarlet", true, "I was in the Conservatory at the time of the merder, but I did notice that Mr. Green was suprizingly calm when everyone esle seemed in compleat shock. That is all I can tell you.", 0, "images/white.jpg", "");
 let white = new Person("Mrs. White", false, "Thank you for helping me calm down. I was so freaked out because while I was cleaning I found ton of blood in the billard room. I know the body was found in a closet in the dinning room, so I'm not sure why there is blood here. Unless he was killed here.", 2, "images/white.jpg", "Im sorry Im just to scared, I cant talk to you. The only thing that calms me down is the flute.");
@@ -103,7 +104,7 @@ let plum = new Person("Professor Plum", true, "I knew him very well. We always u
 let mustard = new Person("Cololel Mustard", false, "Thanks for that drink, ever since the war, when ever I hear a gun shot it brings me back to the times of war. I dont know who could do such a thing and kill him, but the ony people who know how to use a gun are me and Mr. Green.", 6, "images/mustard.jpg", "I dont talk to any one with out a bottle of good wine.");
 let green = new Person("Mr. Green", true, "You are wasting your time talking to me, talk to the other people to find the real merderer. In the mean time im going to finish making my dinner.", 8, "images/green.jpg", "");
 
-const personArray = { 0: scarlet, 1: null, 2: white, 3: peacock, 4: null, 5: plum, 6: mustard, 7: null, 8: green }
+const personArray = { 0: scarlet, 1: null, 2: white, 3: peacock, 4: null, 5: plum, 6: mustard, 7: null, 8: green };
 
 helpText = "You can enter: " + actions[0];
 for (let i = 1; i < actions.length; i++) {
@@ -114,10 +115,11 @@ helpText += ".";
 displayRoom();
 introHelp.textContent = helpText;
 
+// eslint-disable-next-line no-unused-vars
 function getValue() {
-    guessPerson = document.querySelector('select[name="guiltyName"]').value;
-    guessPlace = document.querySelector('select[name="guiltyPlace"]').value;
-    guessWepon = document.querySelector('select[name="guiltyWepon"]').value;
+    guessPerson = document.querySelector("select[name='guiltyName']").value;
+    guessPlace = document.querySelector("select[name='guiltyPlace']").value;
+    guessWepon = document.querySelector("select[name='guiltyWepon']").value;
 
 }
 
@@ -157,6 +159,7 @@ function hasKey() {
 
 function hint(room) {
     switch (room) {
+
         case 0:
         case 2:
         case 3:
@@ -187,10 +190,10 @@ function talkToPerson() {
         personImgPrint.innerHTML = "<img src= \"" + personArray[curentLocation].picture + "\" alt=\"" + personArray[curentLocation].name + "\"" + "height=\"200\" width=\"200\" " + ">";
 
         if (personArray[curentLocation].happy === true) {
-            typeWriterHappy()
+            typeWriterHappy();
 
         } else {
-            typeWriterNotHappy()
+            typeWriterNotHappy();
         }
     }
 }
@@ -207,13 +210,13 @@ function changeRoom() {
     event.preventDefault();
     gameMessage.textContent = "";
     hintPrint.textContent = "";
-    let roomNumber = document.querySelector('input[name="roomNumber"]').value;
+    let roomNumber = document.querySelector("input[name='roomNumber']").value;
     for (let i = 0; i < actions.length; i++) {
         if (roomNumber === actions[i]) {
             valadateMovment(roomNumber);
             break;
         } else if (i === actions.length - 1) {
-            gameMessage.textContent = "Sorry, I did not understand that..."
+            gameMessage.textContent = "Sorry, I did not understand that...";
         }
     }
     document.forms["form1"].reset();
@@ -252,7 +255,7 @@ function unlockDoor() {
         if (unlocked === false) {
             if (curentLocation === 1 || curentLocation === 5) {
                 unlocked = true;
-                gameMessage.textContent = "You have unlocked the door."
+                gameMessage.textContent = "You have unlocked the door.";
             } else {
                 gameMessage.textContent = "There are no doors that need a key here...";
             }
@@ -309,10 +312,10 @@ function valadateMovment(choice) {
             }
             break;
         case "talk":
-            talkToPerson()
+            talkToPerson();
             break;
         case "take secret passage":
-            takePassage()
+            takePassage();
             break;
         case "help":
             gameMessage.textContent = helpText;
